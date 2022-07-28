2116dcae13ea0690dc46e1f0916d25228d33903c
cthermolia-grnet <cthermolia@admin.grnet.gr>
2022-07-28 13:42:42 +0300

    PCC-35 Create Prefix Details view

--- src/components/Prefixes.js
+++ src/components/Prefixes.js
@@ -49,16 +49,19 @@ const Prefixes = () => {
       {
         id: "action",
         cell: props => (
+          
           <div className="edit-buttons">
-            <Link className="btn btn-light btn-sm ml-1 mr-1" to={"#"} >
+            <Link className="btn btn-light btn-sm ml-1 mr-1" to={`/prefixes/${props.row.original.id}`} >
               <FontAwesomeIcon icon="list" />
             </Link>
           </div>
         ),
+        
         header: () => <span>Description</span>,
         footer: null,
         enableColumnFilter: false,
-      },
+      }
+      ,
     ],
     []
   );
@@ -72,15 +75,51 @@ const Prefixes = () => {
 
 const PrefixDetails = () => {
   let params = useParams();
-  
+  const [prefix, setPrefix] = useState([]);
+
+  useEffect(() => {
+    console.log("in use effect prefix details")
+    let DM = new DataManager("localhost:8080/v1");
+    DM.getPrefixes(params.id).then((response) => setPrefix(response));
+  }, []);
+
   if (isNaN(Number(params.id))) {
     return (<Navigate to="/" replace={true} />);
   }
 
-  return (<span>Prefix: {params.id}</span>)
+  return (
+
+    <form>
+  <label>
+    Name: {prefix && prefix.name}
+  </label>
+  <br></br>
+  <label>
+    Service: {prefix && prefix.service_name}
+  </label>
+  <br></br>
+  <label>
+    Provider: {prefix && prefix.provider_name}
+  </label>
+  <br></br>
+  <label>
+    Domain: {prefix && prefix.domain_name}
+  </label>
+  <br></br>
+  
+  <label>
+    Owner: {prefix && prefix.owner}
+  </label>
+  <br></br>
+  <label>
+    Used By: {prefix && prefix.used_by}
+  </label>
+</form>
+ )
 }
 
 const PrefixAdd = () => {
+  
   return (<span>Add</span>)
 }
 
