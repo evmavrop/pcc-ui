8761dc261c9a4415668dcafe3f226536706f89ed
Themis Zamani <themiszamani@gmail.com>
2024-02-12 09:48:33 +0200

    Update info.js
    Labels and descriptions,

--- src/components/Prefixes/info.js
+++ src/components/Prefixes/info.js
@@ -1,51 +1,51 @@
 var prefix = {
     name: {
-        label: "Name",
-        info: "1"
+        label: "Prefix Name",
+        info: "This is the prefix name"
     },
     owner: {
         label: "Owner",
-        info: "2"
+        info: "The Organization that requested the prefix"
     },
     contact_name: {
         label: "Contact Name",
-        info: "3"
+        info: "The name of the contact that requested the prefix"
     },
     contact_email: {
         label: "Contact Email",
-        info: "4"
+        info: "The email of the contact that requested the prefix"
     },
     used_by: {
         label: "Used by",
-        info: "5"
+        info: "The project, service, organization that is using the prefix. Information and small description of the use of prefix"
     },
     service: {
         label: "Service",
-        info: "6"
+        info: "If the prefix is used by a b2 service please select it."
     },
     provider: {
         label: "Provider",
-        info: "7"
+        info: "The name of the Provider that is hosting the service."
     },
     domain: {
         label: "Domain",
-        info: "8"
+        info: "The branch of science, scientific discipline that is related to the use of Prefix. Here we use the classification of the Marketplace ."
     },
     contract_end_date: {
         label: "Contact End Date",
-        info: "9"
+        info: "If the prefix is based on a signed contract then define the contract end date."
     },
     contract_type: {
         label: "Contract Type",
-        info: "10"
+        info: "Select the type of collaboration ."
     },
     lookup_type: {
         label: "LookUp Type",
-        info: "11"
+        info: "Define the way we can lookup the information of a PID. From the central catalogue, its own instance private, both or none"
     },
     status: {
         label: "Status",
-        info: "12"
+        info: "If the prefix is mirrored by the Central Catalogue."
     },
 
 }
